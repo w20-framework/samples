cef74f3bd58618eae957039929aa90a23f9892cf
Jose Manuel Martínez <jmmartienz@me.com>
2017-09-26 15:57:23 +0200

    updated fragment sample

--- w20-fragment/Gruntfile.js
+++ w20-fragment/Gruntfile.js
@@ -1,4 +1,12 @@
-/* globals module: true */
+/*
+ * Copyright (c) 2013-2016, The SeedStack authors <http://seedstack.org>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/* global module: false, grunt: false, process: false */
 module.exports = function (grunt) {
     'use strict';
 
@@ -6,18 +14,20 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         clean: [
             'bower_components/**',
+            'docs/**',
+            'dist/**',
             'coverage/**'
         ],
+        jshint: {
+            bootstrap: {
+                src: ['modules/**/*.js']
+            }
+        },
         bower: {
             install: {
                 options: {
                     copy: false
                 }
-            }
-        },
-        jshint: {
-            fragment_sample: {
-                src: ['fragment_sample/modules/**/*.js']
             }
         },
         karma: {
@@ -30,21 +40,41 @@ module.exports = function (grunt) {
                 autoWatch: true
             }
         },
+        coveralls: {
+            src: ['coverage/lcov.info'],
+            options: {
+                force: true
+            }
+        },
+        ngdocs: {
+            bootstrap: {
+                src: ['modules/**/*.js', 'index.ngdoc'],
+                title: 'Bootstrap 3',
+                api: true
+            }
+        },
         connect: {
-            server: {
+            docs: {
                 options: {
-                    port: 9001,
-                    base: '.',
+                    port: 9000,
+                    base: 'docs',
                     keepalive: true
                 }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-ngdocs');
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-coveralls');
+    grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.registerTask('default', ['clean', 'bower', 'jshint', 'karma', 'connect']);
+
+    grunt.registerTask('docs', ['ngdocs', 'connect:docs']);
+
+    grunt.registerTask('default', ['jshint', 'bower', 'ngdocs']);
 };
